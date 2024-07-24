6df1083f4e40b2beb1b255467576b901721b8c50
Shaun Curtis <shaun@coldelm.com>
2024-07-24 19:38:30 +0100

    Add project files.

---
+++ SO78788856/Components/Pages/WebService.cs
@@ -0,0 +1,20 @@
+﻿namespace SO78788856.Components;
+
+public record StatusData(string Name, string Status, DateTimeOffset Timestamp);
+
+public class StatusService
+{
+    private List<StatusData> statusData = new List<StatusData>()
+    {
+        new("France", "Loaded", DateTimeOffset.Now.AddDays(-3)),
+        new("Spain", "Loaded", DateTimeOffset.Now.AddDays(-2)),
+        new("Portugal", "Loaded", DateTimeOffset.Now.AddDays(-1)),
+    };
+
+    public async ValueTask<IEnumerable<StatusData>> GetStatus()
+    {
+        statusData.Add(new("Me", "Loading", DateTimeOffset.Now));
+        await Task.Yield();
+        return statusData.AsEnumerable();
+    }
+}
